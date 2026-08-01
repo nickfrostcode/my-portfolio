d51b4943589a5f756f79f35971e25f63484b619a
Nick Frost❄️ <bensonnicholas206@gmail.com>
2026-08-01 21:09:04 +0100

    Update config files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'uniscore.vercel.app',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ttenda.vercel.app',
+      },
+      {
+        protocol: 'https',
+        hostname: 'nickspay.com.ng',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
